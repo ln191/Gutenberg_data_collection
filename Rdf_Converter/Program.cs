f9c2484741a47572bca35bb55389f6184101b75e
Lucas <cph-ln191@cphbusiness.dk>
2017-05-13 17:18:58 +0200

    Made program to read rdf files and create the books csv, author csv and book_author_relation csv, is working on test data

---
+++ Rdf_Converter/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdf_Converter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RdfConverter rdf = new RdfConverter();
+            string[] filePaths = Directory.GetFiles(@"test\", "*.rdf",
+                                         SearchOption.AllDirectories);
+            List<Dictionary<string, string>> Books = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> relations = new List<Dictionary<string, string>>();
+            Dictionary<string, string> bookData = new Dictionary<string, string>();
+            //Dictionary<string, string> bookAuthorRelations = new Dictionary<string, string>();
+            List<string> authors = new List<string>();
+            int i = 0;
+
+            Console.WriteLine("input csv books file path:");
+            string csvBooksPath = Console.ReadLine();
+            Console.WriteLine("input csv authors file path:");
+            string csvAuthorPath = Console.ReadLine();
+            Console.WriteLine("input csv book author relation file path:");
+            string csvBookAuthorRelationPath = Console.ReadLine();
+
+            StringBuilder csvBookBuilder = new StringBuilder();
+            csvBookBuilder.AppendLine("id,title,language");
+            File.AppendAllText(csvBooksPath, csvBookBuilder.ToString());
+            csvBookBuilder.Clear();
+
+            StringBuilder csvBookAuthorRelationBuilder = new StringBuilder();
+            csvBookAuthorRelationBuilder.AppendLine("book_id,author");
+            File.AppendAllText(csvBookAuthorRelationPath, csvBookAuthorRelationBuilder.ToString());
+            csvBookAuthorRelationBuilder.Clear();
+
+            StringBuilder csvAuthorBuilder = new StringBuilder();
+            csvAuthorBuilder.AppendLine("author");
+            File.AppendAllText(csvAuthorPath, csvAuthorBuilder.ToString());
+            csvAuthorBuilder.Clear();
+
+            foreach (string path in filePaths)
+            {
+                bookData = rdf.RdfReader(path);
+                if (bookData != null)
+                {
+                    //remove newline from title string
+                    bookData["Title"] = bookData["Title"].Replace("\r\n", "  ");
+
+                    //if title has comma in the string, the string will be surrounded by double quotes
+                    if (bookData["Title"].Contains(","))
+                    {
+                        bookData["Title"] = "\"" + bookData["Title"].Replace("\r\n", "  ") + "\"";
+                    }
+
+                    if (!authors.Contains(bookData["Author"]))
+                    {
+                        authors.Add(bookData["Author"]);
+                    }
+
+                    //add Book id and corresponding author to a relation dictionary
+                    Dictionary<string, string> bookAuthorRelations = new Dictionary<string, string>();
+                    bookAuthorRelations.Add("BookID", bookData["ID"]);
+                    bookAuthorRelations.Add("Author", bookData["Author"]);
+                    relations.Add(bookAuthorRelations);
+
+                    //remove author from book data since we do not want it in our books csv
+                    bookData.Remove("Author");
+                    //add it to a list of books
+                    Books.Add(bookData);
+                }
+
+                i++;
+                if (i == 5)
+                {
+                    //add books to books csv
+                    foreach (var book in Books)
+                    {
+                        string[] rowData = book.Values.ToArray();
+                        csvBookBuilder.AppendLine(String.Join(",", rowData));
+                    }
+                    File.AppendAllText(csvBooksPath, csvBookBuilder.ToString());
+                    csvBookBuilder.Clear();
+                    Books.Clear();
+
+                    //add book author relations to csv file
+                    foreach (var relation in relations)
+                    {
+                        string[] relationData = relation.Values.ToArray();
+                        csvBookAuthorRelationBuilder.AppendLine(String.Join(",", relationData));
+                    }
+                    File.AppendAllText(csvBookAuthorRelationPath, csvBookAuthorRelationBuilder.ToString());
+                    csvBookAuthorRelationBuilder.Clear();
+                    relations.Clear();
+
+                    i = 0;
+                    Console.WriteLine("added 5000 to csv");
+                }
+                //Console.WriteLine(path);
+            }
+            //add books to books csv
+            foreach (var author in authors)
+            {
+                csvAuthorBuilder.AppendLine(author);
+            }
+            File.AppendAllText(csvAuthorPath, csvAuthorBuilder.ToString());
+            csvAuthorBuilder.Clear();
+            Console.WriteLine("csv file has been created, is Done!!!");
+            Console.Read();
+        }
+    }
+}
---
+++ Rdf_Converter/RdfConverter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Rdf_Converter
+{
+    public class RdfConverter
+    {
+        public Dictionary<string, string> RdfReader(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+
+            XNamespace pgterms = "http://www.gutenberg.org/2009/pgterms/";
+            XNamespace dcterms = "http://purl.org/dc/terms/";
+            XNamespace rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+            var values = (from level in doc.Descendants(pgterms + "ebook")
+                          select new
+                          {
+                              Title = (string)level.Element(dcterms + "title"),
+                              Author = (string)level.Elements(dcterms + "creator")
+                                                       .Elements(pgterms + "agent")
+                                                       .Elements(pgterms + "name")
+                                                       .FirstOrDefault(),
+                              Language = (string)level.Elements(dcterms + "language")
+                                                       .Elements(rdf + "Description")
+                                                       .Elements(rdf + "value")
+                                                       .FirstOrDefault(),
+                              Type = (string)level.Elements(dcterms + "type")
+                                                       .Elements(rdf + "Description")
+                                                       .Elements(rdf + "value")
+                                                       .FirstOrDefault()
+                          }).ToList();
+            if (values[0].Type != "Text" || values[0].Language != "en")
+            {
+                return null;
+            }
+            else
+            {
+                // get folder name
+                string lastFolderName = Path.GetFileName(Path.GetDirectoryName(filePath));
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                data.Add("ID", lastFolderName);
+                if (values[0].Title == null)
+                {
+                    data.Add("Title", "Unknown Title");
+                }
+                else
+                {
+                    data.Add("Title", values[0].Title);
+                }
+                if (values[0].Author == null)
+                {
+                    data.Add("Author", "Unknown Author");
+                }
+                else
+                {
+                    data.Add("Author", values[0].Author);
+                }
+                data.Add("Language", values[0].Language);
+
+                return data;
+            }
+        }
+
+        public void WriteToCSVFile(string csvPath, string[] data)
+        {
+            //add data to csv file
+            StringBuilder csvContent = new StringBuilder();
+            csvContent.AppendLine(String.Join(",", data));
+            File.AppendAllText(csvPath, csvContent.ToString());
+            csvContent.Clear();
+        }
+
+        //public string GetAuthor()
+        //{
+        //    XDocument doc = XDocument.Load(filePath);
+
+        //    XNamespace pgterms = "http://www.gutenberg.org/2009/pgterms/";
+        //    XNamespace dcterms = "http://purl.org/dc/terms/";
+        //    XNamespace rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+        //    var values = (from level in doc.Descendants(pgterms + "ebook")
+        //                  select new
+        //                  {
+        //                      Title = (string)level.Element(dcterms + "title"),
+        //                      Author = (string)level.Elements(dcterms + "creator")
+        //                                               .Elements(pgterms + "agent")
+        //                                               .Elements(pgterms + "name")
+        //                                               .FirstOrDefault(),
+        //                      Language = (string)level.Elements(dcterms + "language")
+        //                                               .Elements(rdf + "Description")
+        //                                               .Elements(rdf + "value")
+        //                                               .FirstOrDefault(),
+        //                      Type = (string)level.Elements(dcterms + "type")
+        //                                               .Elements(rdf + "Description")
+        //                                               .Elements(rdf + "value")
+        //                                               .FirstOrDefault()
+        //                  }).ToList();
+        //    if (values[0].Type != "Text" || values[0].Language != "en")
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        if (values[0].Author == null)
+        //        {
+        //            data.Add("Author", "Unknown Author");
+        //        }
+        //        else
+        //        {
+        //            data.Add("Author", values[0].Author);
+        //        }
+        //    }
+        //}
+    }
+}
