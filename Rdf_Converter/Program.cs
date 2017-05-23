843703a0a9b3c1f6ebb0bc5a79f57a43910caac8
Lucas <cph-ln191@cphbusiness.dk>
2017-05-23 14:47:23 +0200

    update rdf program

--- Rdf_Converter/Program.cs
+++ Rdf_Converter/Program.cs
@@ -53,7 +53,7 @@ namespace Rdf_Converter
                     bookData["Title"] = bookData["Title"].Replace("\n", "  "); //linux
                     bookData["Title"] = bookData["Title"].Replace("\r", "  "); //mac
                     bookData["Title"] = bookData["Title"].Replace("\"", "  ");
-
+                    bookData["Author"] = bookData["Author"].Replace("\"", "  ");
                     //if title has comma in the string, the string will be surrounded by double quotes
                     if (bookData["Title"].Contains(","))
                     {
